53d9dc2114eb79c41bafb35a398d16dfa0ad7236
Alex Fotopoulos <alex.foto.dev@gmail.com>
2023-05-04 15:18:34 -0400

    revise business hours

--- components/utilities/OfficeHours.js
+++ components/utilities/OfficeHours.js
@@ -15,12 +15,12 @@ export default function OfficeHours() {
                     <div>Sunday:</div>
                 </div>
                 <div className={styles.officeHoursTime}>
-                    <div>9:00am - 8:00pm</div>
-                    <div>9:00am - 8:00pm</div>
-                    <div>9:00am - 4:00pm</div>
-                    <div>9:00am - 4:00pm</div>
-                    <div>9:00am - 8:00pm</div>
-                    <div>Closed</div>
+                    <div>9:00am - 9:00pm</div>
+                    <div>9:00am - 9:00pm</div>
+                    <div>9:00am - 9:00pm</div>
+                    <div>9:00am - 9:00pm</div>
+                    <div>9:00am - 9:00pm</div>
+                    <div>9:00am - 9:00pm</div>
                     <div>Closed</div>
                 </div>
             </div>
