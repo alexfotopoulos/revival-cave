4038c935f2e920f96929545a518637a91cf86138
Alex Fotopoulos <alex@Alexs-MacBook-Pro.local>
2024-10-26 17:43:05 -0400

    fix images issues from previous commit

--- helpers/therapists.js
+++ helpers/therapists.js
@@ -1,9 +1,14 @@
+
+export const getImageUrl = imagePath => {
+  return `${process.env.NEXT_PUBLIC_BASE_URL || "" }${imagePath}`
+}
+
 export const therapists = [
   {
     id: 1,
     name: "ADAM",
     slug: "adam",
-    image: "/AdamHeadShotNew.jpeg",
+    image: getImageUrl("/AdamHeadShotNew.jpeg"),
     pushImageUp: false,
     bio: ["An Ocala native, Adam began his fitness journey at the age of 13 after discovering his love for weight training. He competed in weightlifting competitions in high school and became a certified personal trainer while attending Florida State University.", "Throughout his fitness journey, Adam incurred numerous injuries including herniated discs, tendinitis, and a ruptured pectoral muscle. Though these injuries were mentally frustrating, Adam came to learn that receiving regular massage and body work would help prevent injury, improve muscle growth and healing, and help him get back to what he loved best - training.", "After competing in a bodybuilding show in 2015, Adam broadened his fitness routine, incorporating yoga, Pilates, cycling, and circuit training. Each program taught him a little bit more about the body, its true biomechanical function, and most importantly, the need to rest, recover and heal.", "With this vast amount of knowledge and experience in the fitness industry, Adam became a licensed massage therapist and opened the Revival Cave to help others heal and perform at their best."]
   },
@@ -11,7 +16,7 @@ export const therapists = [
     id: 2,
     name: "JUSTIN",
     slug: "justin",
-    image: "/JustinHeadShot.jpg",
+    image: getImageUrl("/JustinHeadShot.jpg"),
     pushImageUp: true,
     bio: ["Justin was born in Spain due to his father being in the military, but all his family hails from Boston naturally making him a Red Sox fan.", "Raised in Tampa in the 90's Justin has watched the growth of Tampa happen right before his eyes. After breaking his ankle, Justin found his passion for healing through massage therapy.", "While in school to receive his Massage therapist License, Justin doubled down and signed up to go through a 200-hour Yoga Teacher Training course to further better his understanding of breathwork, movement, and how to combine that knowledge with Massage Therapy. Outside of the massage room you may find Justin enjoying a yoga flow, picking at his guitar, or enjoying a round of golf with his father!"]
   },
@@ -19,7 +24,7 @@ export const therapists = [
     id: 3,
     name: "ALEK",
     slug: "alek",
-    image: "/AlekHeadShot.jpg",
+    image: getImageUrl("/AlekHeadShot.jpg"),
     pushImageUp: true,
     bio: ["Father of two, Alek lived in New York, California, and North Carolina before moving to Tampa, FL in 2017. Alek's love for wrestling and weightlifting as a teen became his gateway to finding his passion in the health and wellness industry.", "Following his dreams, Alek became a certified personal trainer, coaching clients to live a healthier lifestyle. With this experience, as well as flirting with an office job for a few years, Alek learned first-hand what hard labor can do to our bodies, and how even office environments can take a toll on our mental and physical health.", "After becoming a father in 2020 during a global pandemic, stress was at an all-time high. Alek began to take health and wellness to a new level. He knew he wanted to be as healthy and strong as possible for his family. He became a Licensed Massage Therapist to help others understand the power of self-care. His goal is to help others feel revived and empowered both mentally and physically so clients can continue to be their best self they can be!"]
   },
--- src/components/about/HeadShot.js
+++ src/components/about/HeadShot.js
@@ -1,15 +1,22 @@
 import styles from "./HeadShot.module.scss";
+import Image from "next/image";
 
 export default function HeadShot(props) {
-  let imageBottom = props.pushImageUp ? "bottom" : ""
-
+  let imageBottom = props.pushImageUp ? "bottom" : "";
   return (
     <a href={`/about/${props.slug}`}>
-    <div
-      className={styles.HeadShot}
-      style={{ backgroundImage: `linear-gradient(rgb(0, 0, 0, 0.4), rgb(0, 0, 0, 0.4)), url(${props.image})`, backgroundPosition: `${imageBottom}` }}>
-      <p className={styles.HeadShotImageName}>{props.name}</p>
-    </div>
+      <div className={styles.HeadShot}>
+        <Image
+          src={props.image}
+          fill={true}
+          objectFit="cover"
+          className={styles.HeadShotImage}
+          alt={`photo of ${props.name}`}
+          priority
+          style={{objectPosition: props.pushImageUp ? "bottom" : "top"}}
+        />
+        <p className={styles.HeadShotImageName}>{props.name}</p>
+      </div>
     </a>
   );
 }
